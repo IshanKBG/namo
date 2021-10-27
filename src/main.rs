1ec750cd969cea30dc7e1964603a2bde0d78d3e7
IshanKBG <jaiswalishan701@gmail.com>
2021-10-27 12:14:46 +0530

    feat(main.rs): now it can ran on a bare metal system

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+use core::panic::PanicInfo;
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+#[no_mangle]
+pub extern "C" fn _main() -> ! {
+    loop {}
 }
